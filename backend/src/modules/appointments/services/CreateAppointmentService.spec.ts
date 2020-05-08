40bd24f089cd5ba23b410d04966f7ef1d95d16d4
Filipe <ribeiro.filipe94@gmail.com>
2020-05-07 22:46:20 -0300

    feat: added jest config to start tests in backend

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointmentService', () => {
+  it('should be able to create a new appointment', () => {
+
+  });
+
+  it('should not be able to create two appointments on the same time', () => {
+    expect(2 + 2).toBe(true);
+  });
+});
